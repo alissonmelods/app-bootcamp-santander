249c6a1bd0ddfe6c73f30bf56650bccd75af0276
Alisson Melo <alissonmelo.ds@gmail.com>
2021-06-01 01:18:09 -0300

    Correção url serviço

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -7,7 +7,7 @@ import Stock from '../shared/models/stock-model';
 })
 export class DashboardService {
 
-  readonly baseUrl = 'http://alisson-bootcamp-dio-santander.herokuapp.com/bootcamp';
+  readonly baseUrl = 'https://alisson-bootcamp-dio-santander.herokuapp.com/bootcamp';
 
   constructor(
     private http: HttpClient
